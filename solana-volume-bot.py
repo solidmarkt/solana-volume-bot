934018e7ba9eb87a636f10f4e4cf990f25d1ece6
Solid Marketing <189283094+solidmarkt@users.noreply.github.com>
2024-11-29 13:43:48 +0300

    Add files via upload

---
+++ solana-volume-bot.py
@@ -0,0 +1,12 @@
+#This file has no features, please review the README.MD file.
+
+print("Please read README.MD file for details!")
+
+print("Please read README.MD file for details!")
+
+print("Please read README.MD file for details!")
+
+print("Please read README.MD file for details!")
+
+
+#Solana volume bot, read README.MD / contact us via "t.me/solidmarketing"
